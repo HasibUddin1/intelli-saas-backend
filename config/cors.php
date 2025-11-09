168d115b5432b485c60d89a5f8757fc0d6d0409b
HasibUddin1 <uhasib959@gmail.com>
2025-11-10 00:16:47 +0600

    fixed database pricing loading issue

--- config/cors.php
+++ config/cors.php
@@ -6,9 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
-
-    'allowed_origins_patterns' => [],
+    'allowed_origins' => ['http://localhost:3000', '*'],
 
     'allowed_headers' => ['*'],
 
